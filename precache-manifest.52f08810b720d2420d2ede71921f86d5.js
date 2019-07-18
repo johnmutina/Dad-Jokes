65532da7a7113598e378df0ac46d71d61547f6ef
johnmutina <johnmutina@gmail.com>
2019-07-18 23:02:57 +0200

    Updates

---
+++ precache-manifest.52f08810b720d2420d2ede71921f86d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7b6d5a82e656036012c0417defcfa2",
+    "url": "/Dad-Jokes/index.html"
+  },
+  {
+    "revision": "e7547afeaf5dad3afe43",
+    "url": "/Dad-Jokes/static/css/2.760ba37b.chunk.css"
+  },
+  {
+    "revision": "29c57653a4f03a5f845d",
+    "url": "/Dad-Jokes/static/css/main.9c1ba5f5.chunk.css"
+  },
+  {
+    "revision": "e7547afeaf5dad3afe43",
+    "url": "/Dad-Jokes/static/js/2.a34a01e3.chunk.js"
+  },
+  {
+    "revision": "29c57653a4f03a5f845d",
+    "url": "/Dad-Jokes/static/js/main.4ef1d59e.chunk.js"
+  },
+  {
+    "revision": "a2f4470fd54387afb7b5",
+    "url": "/Dad-Jokes/static/js/runtime~main.6318a628.js"
+  }
+]);
